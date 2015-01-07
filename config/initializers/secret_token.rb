e623c041918ed09081902c623a97d8cfd3a8b753
meeravalli <meeravalli.sshaik@gmail.com>
2015-01-07 23:49:15 +0530

    this is first geocode push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geo::Application.config.secret_token = '5a7ee9514950e742474b897427d03acc5f400604ab451c0652eb0d4f2dd31536ca21cece7d658ed46a235b13c6d65b4598ed3d02867c59c931b4cb638d8b9ad6'
